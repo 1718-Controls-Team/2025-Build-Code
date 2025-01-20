6adee9558d6a33ccefe890d0248eb1940782cdb9
1718ControlsTeam <FightingPiElectrical@gmail.com>
2025-01-20 18:47:32 -0500

    coral motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -11,8 +14,42 @@ package frc.robot;
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
+ * public static final double kShooterIntakeRotateMaxForwardVoltage = 11;
+    public static final double kShooterIntakeRotateMaxReverseVoltage = -11;
+    public static final double kShooterIntakeRotateSupplyCurrentLimit = 1; //was 20.  
+    public static final double kShooterIntakeRotateVoltageClosedLoopRampPeriod = 0.1;
+    public static final InvertedValue kShooterIntakeRotateDirection = InvertedValue.CounterClockwise_Positive;
+    public static final double kShooterIntakeRotateMotionMagicCruiseVelocity = 0;
+    public static final double kShooterIntakeRotateMotionMagicAcceleration = 0;
  */
 public final class Constants {
-  
+
+// coralRotate Constants
+  public static final double kCoralRotateProportional = 0; 
+  public static final double kCoralRotateIntegral = 0;
+  public static final double kCoralRotateDerivative = 0;
+  public static final double kCoralRotateFeedFoward = 0;
+  public static final double kCoralRotateVelocityFeedForward = 0;
+  public static final double kCoralRotateGravityFeedForward = 0;
+  public static final double kCoralMaxReverseVoltage = -11;
+  public static final double kCoralMaxForwardVoltage = 11;
+  public static final double kCoralRotateMotionMagicAcceleration = 0;
+  public static final double kCoralRotateMotionMagicCruiseVelocity = 0;
+  public static final double kCoralRotateVoltageClosedLoopRampPeriod = 0;
+  public static final double kCoralRotateSupplyCurrentLimit = 1;
+  public static final InvertedValue kCoralRotateDirection = InvertedValue.Clockwise_Positive;
+  public static final SensorDirectionValue kCoralRotateCancoderDirection = SensorDirectionValue.Clockwise_Positive;
+  public static final double kAbsoluteSensorDiscontinuityPoint = 1;
+
+// coralSpiin Constants
+public static final double kCoralSpinDerivative = 0;
+public static final double kCoralSpinIntegral = 0;
+public static final double kCoralSpinMaxForwardVoltage = 11;
+public static final double kCoralSpinMaxReverseVoltage = -11;
+public static final double kCoralSpinProportional = 0;
+public static final InvertedValue kCoralSpinDirection = InvertedValue.Clockwise_Positive;
+public static final double kCoralSpinSupplyCurrentLimit = 0;
+public static final double kCoralSpinVelocityFeedFoward = 0;
+public static final double kCoralSpinVoltageClosedLoopRampPeriod = 0;
 
 }
--- src/main/java/frc/robot/subsystems/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/CoralIntake.java
@@ -4,27 +4,83 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 
 public class CoralIntake extends SubsystemBase {
   /** Creates a new CoralIntake. */
-  TalonFX coralIntake = new TalonFX(10);
-  TalonFX coralRotate = new TalonFX(10);
+  TalonFX m_coralSpin = new TalonFX(17);
+  TalonFX m_coralRotate = new TalonFX(15);
+  CANcoder m_coralRotatCancoder = new CANcoder(16);
   DutyCycleOut energy = new DutyCycleOut(0);
 
   private final VelocityVoltage coralMotorVelocityRequest = new VelocityVoltage(0);
 
   public CoralIntake() {
-    this.coralIntakeConfiguration(coralIntake);
-    this.coralIntakeConfiguration(coralRotate);
+    this.coralIntakeConfiguration(m_coralSpin);
+    this.coralRotateConfiguration(m_coralRotate);
 
   }
 
-  public void coralIntakeConfiguration(TalonFX coralIntake) {
+  public void coralIntakeConfiguration(TalonFX m_coralSpin) {
+    TalonFXConfiguration coralSpinConfig = new TalonFXConfiguration();
+    Slot0Configs slot0 = coralSpinConfig.Slot0;
+      slot0.kP = Constants.kCoralSpinProportional; // An error of 1 rotation per second results in 2V output
+      slot0.kI = Constants.kCoralSpinIntegral; // An error of 1 rotation per second increases output by 0.5V every second
+      slot0.kD = Constants.kCoralSpinDerivative; // A change of 1 rotation per second squared results in 0.01 volts output
+      slot0.kV = Constants.kCoralSpinVelocityFeedFoward; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second Peak output of 8 volts
+    coralSpinConfig.Voltage.PeakForwardVoltage = Constants.kCoralSpinMaxForwardVoltage;
+    coralSpinConfig.Voltage.PeakReverseVoltage = Constants.kCoralSpinMaxReverseVoltage;
+    coralSpinConfig.CurrentLimits.SupplyCurrentLimit = Constants.kCoralSpinSupplyCurrentLimit;
+    coralSpinConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = Constants.kCoralSpinVoltageClosedLoopRampPeriod;
+    coralSpinConfig.MotorOutput.Inverted = Constants.kCoralSpinDirection;
+    coralSpinConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+  }
+  public void configureCoralCancoder(CANcoder coralRotateCancoder){  
+    CANcoderConfiguration coralRotateCANcoderConfig = new CANcoderConfiguration();
+    coralRotateCANcoderConfig.MagnetSensor.MagnetOffset = -0.398926;
+    coralRotateCANcoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = Constants.kAbsoluteSensorDiscontinuityPoint;
+    coralRotateCANcoderConfig.MagnetSensor.SensorDirection = Constants.kCoralRotateCancoderDirection;
+  }
+  public void coralRotateConfiguration(TalonFX m_coralRotate) {
+    TalonFXConfiguration coralRotateConfig = new TalonFXConfiguration();
+
+  coralRotateConfig.MotionMagic.MotionMagicAcceleration = Constants.kCoralRotateMotionMagicAcceleration;
+  coralRotateConfig.MotionMagic.MotionMagicCruiseVelocity = Constants.kCoralRotateMotionMagicCruiseVelocity;
+  coralRotateConfig.MotorOutput.Inverted = Constants.kCoralRotateDirection;
+  coralRotateConfig.CurrentLimits.SupplyCurrentLimit = Constants.kCoralRotateSupplyCurrentLimit;
+  coralRotateConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = Constants.kCoralRotateVoltageClosedLoopRampPeriod;
+  coralRotateConfig.Voltage.PeakForwardVoltage = Constants.kCoralMaxReverseVoltage;
+  coralRotateConfig.Voltage.PeakForwardVoltage = Constants.kCoralMaxForwardVoltage;
+
+  Slot0Configs slot0 = coralRotateConfig.Slot0;
+    slot0.kP = Constants.kCoralRotateProportional;
+    slot0.kI = Constants.kCoralRotateIntegral;
+    slot0.kD = Constants.kCoralRotateDerivative;
+    slot0.kG = Constants.kCoralRotateGravityFeedForward;
+    slot0.kV = Constants.kCoralRotateVelocityFeedForward;
+    slot0.GravityType = GravityTypeValue.Arm_Cosine;
+
+StatusCode coralRotStatus = StatusCode.StatusCodeNotInitialized;
+for(int i = 0; i < 5; ++i) {
+  coralRotStatus = m_coralRotate.getConfigurator().apply(coralRotateConfig);
+  if (coralRotStatus.isOK()) break;
+}
+if (!coralRotStatus.isOK()) {
+  System.out.println("Could not configure device. Error: " + coralRotStatus.toString());
+}  
 
   }
 
@@ -35,8 +91,8 @@ public class CoralIntake extends SubsystemBase {
    */
  
   public void setcoralMotorPower(double Erika) {
-    coralIntake.setControl(coralMotorVelocityRequest.withVelocity(Erika));
-    coralRotate.setControl(coralMotorVelocityRequest.withVelocity(Erika));
+    m_coralSpin.setControl(coralMotorVelocityRequest.withVelocity(Erika));
+    m_coralRotate.setControl(coralMotorVelocityRequest.withVelocity(Erika));
     System.out.println(Erika);
     
   }
