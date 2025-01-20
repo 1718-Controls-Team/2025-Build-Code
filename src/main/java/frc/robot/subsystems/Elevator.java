10820b78aaa0efbe1f5b884442fd7a8bd1d479cf
T3chN0va <coltengerm@gmail.com>
2025-01-20 18:49:24 -0500

    Climber and Elevator Subsystems
    Still need to finish with the Elevator subsystem

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,29 +4,31 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
-  TalonFX Elevator1 = new TalonFX(10);
-  TalonFX Elevator2 = new TalonFX(10);
+  TalonFX m_Elevator1 = new TalonFX(13);
+  TalonFX m_Elevator2 = new TalonFX(14);
 
-  DutyCycleOut energy = new DutyCycleOut(0);
-
-  private final VelocityVoltage ElevatorVelocityRequest = new VelocityVoltage(0);
+  private final PositionVoltage ElevatorPositionRequest = new PositionVoltage(0).withSlot(0);
+  private final DutyCycleOut ElevatorVoltageRequest = new DutyCycleOut(0);
 
   public Elevator() {
-    this.ElevatorMotorConfiguration(Elevator1);
-    this.ElevatorMotorConfiguration(Elevator2);
+    this.configureElevator1(m_Elevator1);
+    this.configureElevator2(m_Elevator2);
   }
 
-  public void ElevatorMotorConfiguration(TalonFX Elevator1) {
-
-  }
 
   /**
    * Example command factory method.
@@ -34,21 +36,91 @@ public class Elevator extends SubsystemBase {
    * @return a command
    */
  
-  public void setElevatorPower(double Erika) {
-    Elevator1.setControl(ElevatorVelocityRequest.withVelocity(Erika));
-    Elevator2.setControl(ElevatorVelocityRequest.withVelocity(Erika));
-    System.out.println(Erika);
+  public void setElevatorPosition(double DesiredPosition) {
+    m_Elevator1.setControl(ElevatorPositionRequest.withPosition(DesiredPosition));
+    m_Elevator2.setControl(ElevatorPositionRequest.withPosition(DesiredPosition));
+    //System.out.println(Erika);
     
   }
+
+  public void setClimberZeroOutput() {
+    m_Elevator1.setControl(ElevatorVoltageRequest);
+    m_Elevator2.setControl(ElevatorVoltageRequest);
+  }
+
+  public void configureElevator1(TalonFX elevator1){
+    //Start Configuring Elevator
+    TalonFXConfiguration elevator1Config = new TalonFXConfiguration();
+
+    elevator1Config.MotorOutput.Inverted = Constants.kElevator1Direction;
+    elevator1Config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    elevator1Config.CurrentLimits.SupplyCurrentLimit = Constants.kElevator1SupplyCurrentLimit;
+    elevator1Config.ClosedLoopRamps.VoltageClosedLoopRampPeriod = Constants.kElevator1VoltageClosedLoopRampPeriod;
+    elevator1Config.Voltage.PeakForwardVoltage = Constants.kElevator1MaxForwardVoltage;
+    elevator1Config.Voltage.PeakReverseVoltage = Constants.kElevator1MaxReverseVoltage;
+    elevator1Config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+    Slot0Configs slot0 = elevator1Config.Slot0;
+    slot0.kP = Constants.kElevator1Proportional;
+    slot0.kI = Constants.kElevator1Integral;
+    slot0.kD = Constants.kElevator1Derivative;
+    slot0.GravityType = GravityTypeValue.Elevator_Static;
+    slot0.kV = Constants.kElevator1VelocityFeedFoward;
+    //slot0.kS = Constants.kElevator1StaticFeedFoward; // The value of s is approximately the number of volts needed to get the mechanism moving
+
+    //Setting the config option that allows playing music on the motor during disabled.
+    //elevator1Config.Audio.AllowMusicDurDisable = true;
+ 
+    StatusCode climberStatus = StatusCode.StatusCodeNotInitialized;
+    for(int i = 0; i < 5; ++i) {
+      climberStatus = elevator1.getConfigurator().apply(elevator1Config);
+      if (climberStatus.isOK()) break;
+    }
+    if (!climberStatus.isOK()) {
+      System.out.println("Could not configure device. Error: " + climberStatus.toString());
+    }
+    m_Elevator1.setPosition(0);
+  }
+
+  public void configureElevator2(TalonFX elevator2){
+    //Start Configuring Elevator
+    TalonFXConfiguration elevator2Config = new TalonFXConfiguration();
+
+    elevator2Config.MotorOutput.Inverted = Constants.kElevator1Direction;
+    elevator2Config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    elevator2Config.CurrentLimits.SupplyCurrentLimit = Constants.kElevator1SupplyCurrentLimit;
+    elevator2Config.ClosedLoopRamps.VoltageClosedLoopRampPeriod = Constants.kElevator1VoltageClosedLoopRampPeriod;
+    elevator2Config.Voltage.PeakForwardVoltage = Constants.kElevator1MaxForwardVoltage;
+    elevator2Config.Voltage.PeakReverseVoltage = Constants.kElevator1MaxReverseVoltage;
+    elevator2Config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+    Slot0Configs slot0 = elevator2Config.Slot0;
+    slot0.kP = Constants.kElevator2Proportional;
+    slot0.kI = Constants.kElevator2Integral;
+    slot0.kD = Constants.kElevator2Derivative;
+    slot0.GravityType = GravityTypeValue.Elevator_Static;
+    slot0.kV = Constants.kElevator2VelocityFeedFoward;
+    //slot0.kS = Constants.kElevator1StaticFeedFoward; // The value of s is approximately the number of volts needed to get the mechanism moving
+
+    //Setting the config option that allows playing music on the motor during disabled.
+    //elevator2Config.Audio.AllowMusicDurDisable = true;
+ 
+    StatusCode climberStatus = StatusCode.StatusCodeNotInitialized;
+    for(int i = 0; i < 5; ++i) {
+      climberStatus = elevator2.getConfigurator().apply(elevator2Config);
+      if (climberStatus.isOK()) break;
+    }
+    if (!climberStatus.isOK()) {
+      System.out.println("Could not configure device. Error: " + climberStatus.toString());
+    }
+    m_Elevator2.setPosition(0);
+  }
+
   /**
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/TClimber.java
+++ src/main/java/frc/robot/subsystems/TClimber.java
@@ -4,25 +4,31 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class TClimber extends SubsystemBase {
   /** Creates a new TClimber. */
-  TalonFX TClimberMotor = new TalonFX(10);
-  DutyCycleOut energy = new DutyCycleOut(0);
-
-  private final VelocityVoltage TClimberVelocityRequest = new VelocityVoltage(0);
+  TalonFX m_TClimber = new TalonFX(22);
+  
+  private final MotionMagicVoltage TClimberRotationRequest = new MotionMagicVoltage(0);
+  private final DutyCycleOut TClimberVoltageRequest = new DutyCycleOut(0);
 
   public TClimber() {
-    this.TClimberMotorConfiguration(TClimberMotor);
-  }
-
-  public void TClimberMotorConfiguration(TalonFX TClimberMotor) {
-
+    this.configureTClimber(m_TClimber);
   }
 
   /**
@@ -31,11 +37,60 @@ public class TClimber extends SubsystemBase {
    * @return a command
    */
  
-  public void setTClimberPower(double Erika) {
-    TClimberMotor.setControl(TClimberVelocityRequest.withVelocity(Erika));
-    System.out.println(Erika);
+  public void setTClimberPosition(double DesiredPosition) {
+    m_TClimber.setControl(TClimberRotationRequest.withPosition(DesiredPosition));
+    //System.out.println(DesiredPosition);
     
   }
+  
+  public void configureTClimber(TalonFX frontIntakeSpin){
+    TalonFXConfiguration TClimberConfig = new TalonFXConfiguration();
+
+    TClimberConfig.CurrentLimits.SupplyCurrentLimit = Constants.kTClimberSupplyCurrentLimit;
+    TClimberConfig.ClosedLoopRamps.VoltageClosedLoopRampPeriod = Constants.kTClimberVoltageClosedLoopRampPeriod;
+    TClimberConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    TClimberConfig.MotorOutput.Inverted = Constants.kTClimberDirection;
+    TClimberConfig.Voltage.PeakForwardVoltage = Constants.kTClimberMaxForwardVoltage;
+    TClimberConfig.Voltage.PeakReverseVoltage = Constants.kTClimberMaxReverseVoltage;
+    TClimberConfig.MotionMagic.MotionMagicAcceleration = Constants.kTClimberMotionMagicAcceleration;
+    TClimberConfig.MotionMagic.MotionMagicCruiseVelocity = Constants.kTClimberMotionMagicCruiseVelocity;
+    //TClimberConfig.MotionMagic.MotionMagicJerk = Constants.kTClimberMotionMagicJerk; // Idk how needed Jerk is
+    TClimberConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    Slot0Configs slot0 = TClimberConfig.Slot0;
+    slot0.kP = Constants.kTClimberProportional;
+    slot0.kI = Constants.kTClimberIntegral;
+    slot0.kD = Constants.kTClimberDerivative;
+    slot0.kG = Constants.kTClimberGravity;
+    slot0.kV = Constants.kTClimberVelocityFeedFoward;
+    slot0.GravityType = GravityTypeValue.Arm_Cosine;
+
+    //slot0.kV = Constants.kTClimberVelocityFeedFoward;
+    //slot0.kS = Constants.kTClimberStaticFeedFoward; // The value of s is approximately the number of volts needed to get the mechanism moving
+    TClimberConfig.Feedback.FeedbackRemoteSensorID = Constants.kTClimberCancoderCanID;
+    TClimberConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+    TClimberConfig.Feedback.RotorToSensorRatio = Constants.kTClimberRotorToSensorRatio;
+
+    //Setting the config option that allows playing music on the motor during disabled.
+    TClimberConfig.Audio.AllowMusicDurDisable = true;
+
+    StatusCode TClimberStatus = StatusCode.StatusCodeNotInitialized;
+    for(int i = 0; i < 5; ++i) {
+      TClimberStatus = m_TClimber.getConfigurator().apply(TClimberConfig);
+      if (TClimberStatus.isOK()) break;
+    }
+    if (!TClimberStatus.isOK()) {
+      System.out.println("Could not configure device. Error: " + TClimberStatus.toString());
+    }
+  }
+
+  public void configureFrontIntakeCancoder(CANcoder frontIntakeCancoder){  
+    CANcoderConfiguration TClimberCANcoderConfig = new CANcoderConfiguration();
+    TClimberCANcoderConfig.MagnetSensor.MagnetOffset = -0.398926;
+    TClimberCANcoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = Constants.kTClimberCancoderAbsoluteSensorDiscontinuityPoint
+    TClimberCANcoderConfig.MagnetSensor.SensorDirection = Constants.kFrontIntakeRotateCancoderDirection;
+  }
+  
   /**
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
    *
