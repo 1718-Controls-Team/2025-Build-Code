61b1e71cb0a36ab7e20b00039da5df90d883c768
1718ControlsTeam <FightingPiElectrical@gmail.com>
2025-01-22 12:25:47 -0500

    commands

---
+++ src/main/java/frc/robot/commands/AlgaeDelivery.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.AlgaeIntake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+/** An example command that uses an example subsystem. */
+public class AlgaeDelivery extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+ 
+
+  /**
+   * Creates a new set-PowerCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public AlgaeDelivery(AlgaeIntake subsystem) {
+    
+   
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  // might need something about the subsystem here
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
