1f732abb2244737db2c5085238c2985b0a7e2149
1718ControlsTeam <FightingPiElectrical@gmail.com>
2025-01-25 11:54:44 -0500

    things

--- src/main/java/frc/robot/commands/CoralPickup.java
+++ src/main/java/frc/robot/commands/CoralPickup.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.BeamBreak;
+import frc.robot.subsystems.BeamBreakSubsystem;
 import frc.robot.subsystems.CoralIntake;
 import frc.robot.subsystems.Elevator;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,9 +16,8 @@ public class CoralPickup extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final CoralIntake m_coralSubsystem;
   private final Elevator m_elevatorSubsystem;
-  private final BeamBreak m_beamBreakSubsystem;
+  private final BeamBreakSubsystem m_beamBreakSubsystem;
 
-  private int m_stateMachine = 1;
     private boolean m_isFinished = false;
   
   
@@ -27,7 +26,7 @@ public class CoralPickup extends Command {
      *
      * @param subsystem The subsystem used by this command.
      */
-    public CoralPickup(CoralIntake coralSubsystem, Elevator elevatorSubsystem, BeamBreak beamBreakSubsystem) {
+    public CoralPickup(CoralIntake coralSubsystem, Elevator elevatorSubsystem, BeamBreakSubsystem beamBreakSubsystem) {
       m_coralSubsystem = coralSubsystem;
       m_elevatorSubsystem = elevatorSubsystem;
       m_beamBreakSubsystem = beamBreakSubsystem;
@@ -43,7 +42,6 @@ public class CoralPickup extends Command {
         // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-      m_stateMachine = 1;
       m_isFinished = false;
     
     /** if (m_beamBreakSubsystem.getCoralPresent()){ //Check to see if we have a coral present, and end command if we do.
@@ -53,7 +51,7 @@ public class CoralPickup extends Command {
       m_coralSubsystem.setcoralRotate(Constants.kCoralIntakeDownPos);  
       m_coralSubsystem.setcoralSpinPower(Constants.kCoralIntakeSpeed);  
       //m_shooterSubsystem.setShooterSpeed(0); Temporarily disabling this for auto testing
-      m_elevatorSubsystem.setElevatorDesiredPosition(Constants.kElevatorHomePos);
+      m_elevatorSubsystem.setElevatorDesiredPosition(Constants.kElevatorIntakePos);
      
     }
 
@@ -61,37 +59,22 @@ public class CoralPickup extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  // might need something about the subsystem here
-    //State Machine
-    switch(m_stateMachine){     
-      case 1:  //Elevator and Coral rotate
-        if (m_coralSubsystem.getCoralPosition() == Constants.kCoralIntakeDownPos) {
-          m_elevatorSubsystem.setElevatorPosition(Constants.kElevatorIntakePos);
-          m_stateMachine = m_stateMachine + 1;
-        }        
-      break;
-      case 2:  // Coral spin go brrr
-        if (m_elevatorSubsystem.getElevatorPosition() == Constants.kElevatorIntakePos) {
-          m_coralSubsystem.setcoralSpinPower(Constants.kCoralSpinIntakeSpeed);
-          m_stateMachine = m_stateMachine + 1;
-        }
-      break;
-      case 3:  // Coral in 
-        // if (m_beamBreakSubsystem.getNotePresentIntake()) {
-          // maybe not use this?? depends on line up -> m_elevatorSubsystem.setElevatorPosition(Constants.kElevatorHomePos);
-          m_coralSubsystem.setcoralSpinPower(Constants.kCoralSpinStopSpeed);
-          m_isFinished=true;
+ 
         
-      break;
       }
-    }
+
   
   
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
+  public void end(boolean interrupted) {
+  if (m_beamBreakSubsystem.getCoralPresentIntake())
+    m_elevatorSubsystem.setElevatorPosition(Constants.kElevatorHomePos);
+    m_coralSubsystem.setcoralSpinPower(Constants.kCoralSpinStopSpeed);
+    m_isFinished=true;
+  
+}
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
---
+++ src/main/java/frc/robot/subsystems/BeamBreakSubsystem.java
@@ -0,0 +1,99 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+// The shooter intake subsystem is the portion of the shooter that is NOT responsible for ejecting notes at high velocities.
+public class BeamBreakSubsystem extends SubsystemBase {
+
+  //Open sensors
+  AnalogInput m_BeamBreakZeroAnalog = new AnalogInput(Constants.kBeamBreakZeroAnalog);
+  AnalogInput m_BeamBreakOneAnalog = new AnalogInput(Constants.kBeamBreakOneAnalog);
+
+  Debouncer m_IntakeDebounce = new Debouncer(0.05, Debouncer.DebounceType.kBoth);
+  Debouncer m_ShooterDebounce = new Debouncer(0.05, Debouncer.DebounceType.kBoth);
+
+  //Open Servo
+  //Servo intakeHinge = new Servo(Constants.kShooterIntakePivotReleasePWM);
+
+  public BeamBreakSubsystem() {
+  m_BeamBreakZeroAnalog.setAverageBits(6);
+  m_BeamBreakOneAnalog.setAverageBits(6);
+  }
+  
+  // Start of sensor related methods
+  /**
+   * Check if a note is in front of the intake beam break.
+   * @return Whether the intake beam break detects a note.
+   * True or false.
+   */
+  public boolean getCoralPresentIntake() {  
+    if (Constants.kPrintSubsystemBeamBreak){System.out.println("Subsystem: Shooter - getNotePresentShooter Voltage " + m_BeamBreakOneAnalog.getAverageVoltage());}
+    
+    return (m_IntakeDebounce.calculate(m_BeamBreakZeroAnalog.getAverageVoltage() >= Constants.kIntakeBeamBreakCrossover));
+  }
+  
+  /**
+   * Check if a note is in front of the shooter beam break.
+   * @return Whether the shooter beam break detects a note.
+   * True or false.
+   */
+  public boolean getAlgaePresentIntake() {  
+    if (Constants.kPrintSubsystemBeamBreak){
+      System.out.println("Subsystem: Shooter - getNotePresentShooter Voltage " + m_BeamBreakOneAnalog.getAverageVoltage());
+      System.out.println("Subsystem: Shooter - getNotePresentShooter :" + (m_BeamBreakOneAnalog.getAverageVoltage() >= Constants.kShooterBeamBreakCrossover));
+    }
+    return (m_ShooterDebounce.calculate(m_BeamBreakOneAnalog.getAverageVoltage() >= Constants.kShooterBeamBreakCrossover));
+    
+  }
+  // End of sensor related methods
+
+  /**
+   * Check if a note is in front of either the intake beam break or the shooter beam break.
+   * @return Whether either beam break detects a note.
+   * True or false.
+   */
+  public boolean getElementPresent() {
+    if (Constants.kPrintSubsystemBeamBreak){
+      System.out.println("Subsystem: Shooter - getNotePresentShooter Voltage " + m_BeamBreakOneAnalog.getAverageVoltage());
+      System.out.println("Subsystem: Shooter - getNotePresentIntake Voltage " + m_BeamBreakZeroAnalog.getAverageVoltage());
+    }
+    return (this.getCoralPresentIntake() || this.getAlgaePresentIntake());
+  }
+
+  /**
+   * Sets the speed of the shooter intake motor.
+   * @param speed The desired speed of the shooter intake, in rotations per second.
+   */
+
+  /**
+   * Extends the intake hinge servo to allow the intake to pivot.
+   */
+  
+  @Override
+  public void initSendable(SendableBuilder builder){
+    builder.setSmartDashboardType("BeamBreakSubsystem");
+    builder.addBooleanProperty("Coral Present in Intake?", this::getCoralPresentIntake, null); 
+    builder.addBooleanProperty("Algae Present in Intake?", this::getAlgaePresentIntake, null);
+    builder.addDoubleProperty("Intake Beam Break Voltage", () -> {return m_BeamBreakZeroAnalog.getAverageVoltage();}, null);
+    builder.addDoubleProperty("Shooter Beam Break Voltage", () -> {return m_BeamBreakOneAnalog.getAverageVoltage();}, null);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
